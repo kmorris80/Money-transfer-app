8a67c474efccdb1899d58ce7b3699c90a3f7a687
Karen Morris <bykarenmorris@gmail.com>
2022-05-01 16:18:43 -0500

    pushing additions to testing

--- capstone/tenmo-server/src/test/java/com/techelevator/tenmo/dao/JdbcAccountDaoTest.java
+++ capstone/tenmo-server/src/test/java/com/techelevator/tenmo/dao/JdbcAccountDaoTest.java
@@ -14,26 +14,31 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 
 import static org.junit.Assert.*;
-public class JdbcAccountDaoTest {
-    private static SingleConnectionDataSource dataSource;
+public class JdbcAccountDaoTest{
+//    private static SingleConnectionDataSource dataSource;
     private Account testAccount;
     private JdbcAccountDao sut;
 
-    @BeforeClass
-    public static void setupData(){
-        dataSource = new SingleConnectionDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/test_tenmo");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres1");
-        dataSource.setAutoCommit(false);
-    }
+    private static final Account ACCOUNT_1 = new Account(2002, 1002, new BigDecimal("1000,00"));
+    private static final Account ACCOUNT_2 = new Account(2003, 1003, new BigDecimal("1000,00"));
+    private static final Account ACCOUNT_3 = new Account(2004, 1004, new BigDecimal("1000,00"));
+
+
+
+//    @BeforeClass
+//    public static void setupData(){
+//        dataSource = new SingleConnectionDataSource();
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/test_tenmo");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres1");
+//        dataSource.setAutoCommit(false);
+//    }
 
     @Before
     public void setup(){
-        String sql = "INSERT account (account_id, user_id, balance) VALUES (2001, 1001, 1000)" ;
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        jdbcTemplate.update(sql);
-        sut = new JdbcAccountDao(jdbcTemplate);
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//        sut = new JdbcAccountDao(jdbcTemplate);
+          sut = new JdbcAccountDao(dataSource);
     }
 
     @After
@@ -47,9 +52,9 @@ public class JdbcAccountDaoTest {
     }
 
     @Test
-    public void getAccount_returns_correct_account() {
+    public void getAccountById_returns_correct_account() {
         Account account = sut.getAccount(1001);
-        Assert.assertEquals(2001, account.getBalance());
+        Assert.assertEquals(2001, );
 
     }
 
